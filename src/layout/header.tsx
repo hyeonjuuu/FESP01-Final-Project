1fb68c43e3b14254a19db20fba202e5031dcffa2
bomlang <bomlang4211@gmail.com>
2023-11-20 14:32:04 +0900

    [Chore] 폰트어썸 설정

---
+++ src/layout/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function header() {
+  return (
+    <div>
+      <div>
+        <form action="post">
+          <input type="text" />
+          <button type="button"></button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default header;
