6d1516b9d622ab854bd9751c7beb2c414702b558
haha41 <thgml500@gmail.com>
2023-11-20 17:53:15 +0900

    [Fix] 머지 후 라우터 적용안되는 것 수정함

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import "./tailwind.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RootLayout } from "./layout/RootLayout/RootLayout";
+import GetData from "./pages/GetData";
+import GetDetailData from "./pages/GetDetailData";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<GetData />} />
+          <Route path="detail/:id" element={<GetDetailData />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/layout/RootLayout/RootLayout.tsx
+++ src/layout/RootLayout/RootLayout.tsx
@@ -1,6 +1,6 @@
-import React from "react-router-dom";
+import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../header";
+import Header from "../Header";
 
 export const RootLayout = (): JSX.Element => {
   return (
