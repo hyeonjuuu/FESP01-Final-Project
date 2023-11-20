1bbfcb54944968a034b932c14b3adaef835f6d91
haha41 <thgml500@gmail.com>
2023-11-20 16:58:26 +0900

    [Docs] 데이터 인터페이스 정의

---
+++ src/data.d.ts
@@ -0,0 +1,46 @@
+interface VideoData {
+  kind: string;
+  etag: string;
+  items: {
+    etag: string;
+    id: string;
+    kind: string;
+    snippet: {
+      publishedAt: string;
+      channelId: string;
+      title: string;
+      description: string;
+      thumbnails: {
+        default: {
+          url: string;
+          width: number;
+          height: number;
+        };
+      };
+    };
+  }[];
+  nextPageToken: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+}
+
+interface VideoDetailData {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: {
+        url: string;
+        width: number;
+        height: number;
+      };
+    };
+  };
+}
