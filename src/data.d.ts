ab0f6508d1078517c53ef7bc7fa2e8b487d129ea
haha41 <thgml500@gmail.com>
2023-11-20 23:59:35 +0900

    [Feat] 관련된 영상 데이터 가져오기

--- src/data.d.ts
+++ src/data.d.ts
@@ -28,20 +28,20 @@ interface VideoData {
 }
 
 interface VideoDetailData {
-  kind: string;
-  etag: string;
-  id: string;
-  snippet: {
-    publishedAt: string;
-    channelId: string;
-    title: string;
-    description: string;
-    thumbnails: {
-      default: {
-        url: string;
-        width: number;
-        height: number;
+  items: Array<{
+    id: { videoId: string };
+    snippet: {
+      thumbnails: {
+        default: {
+          url: string;
+          width: number;
+          height: number;
+        };
       };
+      channelTitle: string;
+      title: string;
+      description: string;
+      publishedAt: string;
     };
-  };
+  }>;
 }
--- src/pages/GetDetailData.tsx
+++ src/pages/GetDetailData.tsx
@@ -1,47 +1,51 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useLocation } from "react-router";
 import axios from "axios";
 
 function GetDetailData() {
-  const { id } = useParams<{ id: string }>();
-  const [detailData, setDetailData] = useState<VideoDetailData | null>(null);
-
-  const fetchDetailData = async () => {
-    try {
-      const response = await axios.get(
-        `/videos/searchByChannels/search-by-channel-id-${id}.json`
-      );
-      console.log(response.data);
-      setDetailData(response.data);
-    } catch (error) {
-      console.error("Error fetching detail data:", error);
-    }
-  };
+  const location = useLocation();
+  // console.log("location: ", location);
+  const channelId = location.state.channelId;
+  const [detailData, setDetailData] = useState<VideoDetailData>();
 
   useEffect(() => {
+    const fetchDetailData = async () => {
+      try {
+        const response = await axios.get(
+          `/videos/searchByChannels/search-by-channel-id-${channelId}.json`
+        );
+        console.log(response.data);
+        setDetailData(response.data);
+      } catch (error) {
+        console.error("Error fetching detail data:", error);
+      }
+    };
+
     fetchDetailData();
-  }, [id]);
+  }, [channelId]);
 
   return (
     <div>
-      <h2>영상 상세 페이지</h2>
-      {detailData && (
-        <>
+      <p>해당 영상</p>
+
+      <p>관련된 영상</p>
+      {detailData?.items.map((item) => (
+        <div key={item.id.videoId}>
           <img
-            src={detailData.snippet.thumbnails.default.url}
+            src={item.snippet.thumbnails.default.url}
             alt=""
-            width={detailData.snippet.thumbnails.default.width}
-            height={detailData.snippet.thumbnails.default.height}
+            width={item.snippet.thumbnails.default.width}
+            height={item.snippet.thumbnails.default.height}
             aria-labelledby="title"
           />
           <ul>
-            <li>채널이름: {detailData.snippet.channelId}</li>
-            <li id="title">제목: {detailData.snippet.title}</li>
-            <li>상세설명: {detailData.snippet.description}</li>
-            <li>생성날짜: {detailData.snippet.publishedAt}</li>
+            <li>채널이름: {item.snippet.channelTitle}</li>
+            <li id="title">제목: {item.snippet.title}</li>
+            <li>상세설명: {item.snippet.description}</li>
+            <li>생성날짜: {item.snippet.publishedAt}</li>
           </ul>
-        </>
-      )}
+        </div>
+      ))}
     </div>
   );
 }
