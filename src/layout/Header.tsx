bc945ad84d4a896d941741c192f433ec8943b771
haha41 <thgml500@gmail.com>
2023-11-21 11:19:14 +0900

    [Feat] 뒤로가기 버튼 컴포넌트 완성

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,19 +2,27 @@ import React from "react";
 import SearchBar from "../components/SearchBar";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import BackButton from "../components/BackButton";
 
 function Header() {
   return (
-    <div className="flex justify-between px-[1.75rem] pt-[5px] pb-[35px] items-center">
-      <div className="flex items-center gap-5">
-        <img src="/youtubeLogo.svg" alt="로고" className="w-[4rem] h-[3rem]" />
+    <>
+      <div className="flex justify-between px-[1.75rem] pt-[5px] pb-[35px] items-center">
+        <div className="flex items-center gap-5">
+          <img
+            src="/youtubeLogo.svg"
+            alt="로고"
+            className="w-[4rem] h-[3rem]"
+          />
+        </div>
+        <SearchBar />
+        <button className="w-[6rem] h-[2.5rem] border rounded-[35px] border-[#e6e6e6] focus:border-none hover:bg-[#DEF0FE] flex justify-center items-center gap-2">
+          <FontAwesomeIcon icon={faUser} />
+          <span>로그인</span>
+        </button>
       </div>
-      <SearchBar />
-      <button className="w-[6rem] h-[2.5rem] border rounded-[35px] border-[#e6e6e6] focus:border-none hover:bg-[#DEF0FE] flex justify-center items-center gap-2">
-        <FontAwesomeIcon icon={faUser} />
-        <span>로그인</span>
-      </button>
-    </div>
+      <BackButton />
+    </>
   );
 }
 
