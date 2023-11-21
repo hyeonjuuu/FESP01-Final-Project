a44ce20f38b6bc99f18cc8b411cbd0ee9b53225a
haha41 <thgml500@gmail.com>
2023-11-22 01:20:37 +0900

    [Modify] 필요 없는 주석 삭제

--- src/utils/getImage.ts
+++ src/utils/getImage.ts
@@ -1,40 +1,10 @@
 import React from "react";
 import getData from "./getData";
 
-// interface Thumbnail {
-//   url: string;
-//   width: number;
-//   height: number;
-// }
-
-// interface Snippet {
-//   thumbnails: {
-//     default: Thumbnail;
-//     medium: Thumbnail;
-//     high: Thumbnail;
-//     standard: Thumbnail;
-//   };
-// }
-
-// interface Item {
-//   snippet: Snippet;
-// }
-
-// interface VideoData {
-//   kind: string;
-//   etag: string;
-//   items: Item[];
-//   nextPageToken: string;
-//   pageInfo: {
-//     totalResults: number;
-//     resultsPerPage: number;
-//   };
-// }
-
 async function getImage(size: string) {
   const data = (await getData()) as VideoData;
   const items = data.items;
-  // console.log("data.items: ", data.items);
+
   return items.map((item) => {
     switch (size) {
       case "sm":
