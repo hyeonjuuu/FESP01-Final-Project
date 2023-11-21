c85a445c5c3cd39df87523840a56caaad5134c11
bomlang <bomlang4211@gmail.com>
2023-11-21 13:47:42 +0900

    [Design] 헤더 레이아웃 반응형디자인 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,13 @@ export default {
   theme: {
     extend: {},
     screens: {
-      'sm': '320px',
-
-      'md': '768px',
-
-      'lg': '1024px',
-
-      'xl': '1280px',
-
-      '2xl': '1536px',
-    }
+      // 데스크탑
+      pc: { min: "1025px", max: "1920px" },
+      // 테블릿
+      tb: { min: "768px", max: "1024px" },
+      // 모바일
+      mo: { max: "767px", min: "320px" },
+    },
   },
   plugins: [],
 };
