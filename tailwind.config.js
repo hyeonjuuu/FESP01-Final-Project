6ae21ad2036e4277c481e8de83e6dd517a3dcecc
hyeonjuuu <janghyeonjuu@gmail.com>
2023-11-21 09:41:15 +0900

    [Design] tailwind.config 반응형 breakpoint 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@ export default {
   theme: {
     extend: {},
     screens: {
-      'sm': '320px',
+      sm: "320px",
 
-      'md': '768px',
+      md: "768px",
 
-      'lg': '1024px',
+      lg: "1024px",
 
-      'xl': '1280px',
+      xl: "1280px",
 
-      '2xl': '1536px',
-    }
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
