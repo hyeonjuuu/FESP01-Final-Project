851ee9d2b18738cdff92fce5b8ade6944325b1c4
hyeonjuuu <janghyeonjuu@gmail.com>
2023-11-22 11:13:59 +0900

    [Feat] 현재 날짜로 부터 게시 일자까지 경과된 시간 렌더링

--- src/components/VideoComponets.tsx
+++ src/components/VideoComponets.tsx
@@ -8,7 +8,38 @@ function VideoComponets({
   image,
   item,
 }: VideoComponentsProps) {
-  // console.log("size:", size);
+  /*   const today = new Date();
+
+  const publishDay = new Date(item.snippet.publishedAt);
+
+  const passedSec = today.getTime() - publishDay.getTime();
+
+  const passedDay = passedSec / 1000 / 60 / 60 / 24;
+  const passedPublishDate = Math.floor(passedDay) + "일 전";
+  console.log(passedPublishDate); */
+
+  function getPassedDay() {
+    const today = new Date();
+
+    const publishDay = new Date(item.snippet.publishedAt);
+
+    const passedSec = today.getTime() - publishDay.getTime();
+
+    const passedDay = passedSec / 1000 / 60 / 60 / 24;
+    // const passedPublishDate = Math.floor(passedDay) + "일 전";
+
+    if (passedDay < 7 && passedDay < 365) {
+      return Math.floor(passedDay) + "일 전";
+    } else if (passedDay >= 7 && passedDay < 30) {
+      return Math.floor(passedDay / 7) + "주 전";
+    } else if (passedDay >= 30 && passedDay < 365) {
+      return Math.floor(passedDay / 30) + "개월 전";
+    } else {
+      return Math.floor(passedDay / 365) + "년 전";
+    }
+  }
+
+  console.log(getPassedDay());
 
   return (
     <>
@@ -41,13 +72,13 @@ function VideoComponets({
             {detail === "상세설명" ? (
               <dd className="text-base ">{item.snippet.description}</dd>
             ) : detail === "생성날짜" ? (
-              <dd className="text-sm ">{item.snippet.publishedAt}</dd>
+              <dd className="text-sm ">{getPassedDay()}</dd>
             ) : (
               <>
                 <dd className="text-base w-[96%] text-ellipsis overflow-hidden truncate ">
                   {item.snippet.description}
                 </dd>
-                <dd className="text-sm">{item.snippet.publishedAt}</dd>
+                <dd className="text-sm">{getPassedDay()}</dd>
               </>
             )}
           </dl>
