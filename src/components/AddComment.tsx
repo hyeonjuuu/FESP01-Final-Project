9874d9157d27369af685a71088a945eba29ead76
haha41 <thgml500@gmail.com>
2023-11-24 02:10:17 +0900

    [Feat] 댓글입력칸 다시 textarea로 변경

--- src/components/AddComment.tsx
+++ src/components/AddComment.tsx
@@ -1,38 +1,43 @@
 import React, { useEffect, useRef, useState } from "react"
 import Button from "./Button"
-import { focusContentEditableTextToEnd } from "@utils/focusContentEditableTextToend"
 
 function AddComment() {
   const [isState, setIsState] = useState(false)
-  const [content, setContent] = useState<string>("댓글 추가...")
-  const contentRef = useRef<HTMLDivElement>(null)
+  const [text, setText] = useState<string>("")
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
 
   const handleInputFocus = () => {
     setIsState(true)
-    setContent("")
   }
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLDivElement>) => {
-    setContent(e.target.innerText)
-    focusContentEditableTextToEnd(contentRef.current!)
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value)
   }
 
   const handleCancel = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
     setIsState(false)
-    setContent("댓글 추가...")
+    setText("")
   }
 
   const handleCommentSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    console.log("댓글 등록:", content)
-    setContent("")
+    console.log("댓글 등록:", text)
+    setText("")
   }
+
+  // useEffect를 사용하여 텍스트가 변경될 때마다 높이를 조절합니다.
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "30px"
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`
+    }
+  }, [text])
 
   return (
     <div className="w-full pb-2">
       <div className="h-auto flex">
-        <div className="h-auto pr-4">
+        <div className="min-h-[102px] pr-4">
           <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
             <img
               src="/smile.png"
@@ -43,17 +48,14 @@ function AddComment() {
         </div>
 
         <form className=" w-full flex-col gap-10">
-          <div
-            className={`w-full border-b-2 mb-2 focus:outline-none focus:border-b-slate-500 ${
-              content ? "h-auto" : "h-[30px]"
-            }`}
-            ref={contentRef}
-            contentEditable="true"
+          <textarea
+            ref={textareaRef}
+            placeholder="댓글 추가..."
+            className="w-full border-b-2 mb-2 focus:outline-none focus:border-b-slate-500 overflow-hidden"
+            value={text}
             onFocus={handleInputFocus}
-            onInput={handleInputChange}
-          >
-            {content}
-          </div>
+            onChange={handleInputChange}
+          />
 
           {isState && (
             <div className="flex gap-3 justify-end">
@@ -68,9 +70,7 @@ function AddComment() {
                 type={"submit"}
                 onClick={handleCommentSubmit}
                 color={
-                  content
-                    ? "bg-blue-500 text-white"
-                    : "bg-gray-100 text-gray-700"
+                  text ? "bg-blue-500 text-white" : "bg-gray-100 text-gray-700"
                 }
               />
             </div>
