527975e7e23b9eec71d10c7aace0947aeaa1567e
haha41 <thgml500@gmail.com>
2023-11-23 14:51:24 +0900

    [Feat] 댓글 input, button 기능 일부 구현

--- src/components/AddComment.tsx
+++ src/components/AddComment.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 function AddComment() {
+  const [text, setText] = useState<string>("");
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleCancel = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Button이 클릭되었습니다.");
+  };
+
+  const handleCommentSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("댓글 등록:", text);
+    setText("");
+  };
+
   return (
     <div className="w-full pb-2">
       <div className="max-h-[142px] flex">
@@ -20,10 +36,16 @@ function AddComment() {
             type="text"
             placeholder="댓글 추가..."
             className="w-full border-b-2 mb-2"
+            value={text}
+            onChange={handleInputChange}
           />
-          <div className="flex gap-3 justify-end pr-2">
-            <Button text={"취소"} type={"button"} />
-            <Button text={"등록"} type={"submit"} />
+          <div className="flex gap-3 justify-end">
+            <Button text={"취소"} type={"button"} onClick={handleCancel} />
+            <Button
+              text={"등록"}
+              type={"submit"}
+              onClick={handleCommentSubmit}
+            />
           </div>
         </form>
       </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,18 +3,19 @@ import React from "react";
 interface ButtonProps {
   text: string;
   type: "button" | "submit" | "reset";
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-function Button({ text, type }: ButtonProps) {
-  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    console.log("버튼 타입: ", type);
-  };
+function Button({ text, type, onClick }: ButtonProps) {
+  // const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   e.preventDefault();
+  //   console.log("버튼 타입: ", type);
+  // };
   return (
     <button
       type={type}
       className="flex items-center justify-center bg-gray-200 py-2 px-3 rounded-full"
-      onClick={handleButtonClick}
+      onClick={onClick}
     >
       {text}
     </button>
