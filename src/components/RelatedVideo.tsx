faafb0f5cc605a117ab7e732f22a32e7cbdc6602
haha41 <thgml500@gmail.com>
2023-11-23 23:09:44 +0900

    [Design] videoDetail 페이지 이미지 테두리 라운드, 패딩 적용

--- src/components/RelatedVideo.tsx
+++ src/components/RelatedVideo.tsx
@@ -12,18 +12,18 @@ function RelatedVideo({ item, date }: RelatedVideoProps) {
   return (
     <>
       <Link to={`/videoDetail/${item.id}`} state={{ item: item }}>
-        <div className="flex">
+        <div className="flex pb-2">
           <div className="min-w-[168px] h-[94px]">
             <img
               src={item.snippet.thumbnails.high.url}
               alt={item.snippet.title}
-              className="w-full h-full border-neutral-500 border-[0.5px] object-cover"
+              className="w-full h-full border-neutral-500 border-[0.5px] object-cover rounded-lg"
             ></img>
           </div>
 
-          <div className="pt-2 mo:w-[70%] tb:w-full min-w-[360px]">
+          <div className="pl-2 mo:w-[70%] tb:w-full min-w-[360px]">
             <dl className="flex flex-col">
-              <dt className="text-lg font-semibold text-ellipsis overflow-hidden truncate w-full">
+              <dt className="text-base font-semibold text-ellipsis overflow-hidden truncate w-full">
                 {item.snippet.title}
               </dt>
               <dd className="text-sm">{item.snippet.channelTitle}</dd>
