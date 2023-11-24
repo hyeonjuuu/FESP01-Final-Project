b9e092e46ca73efabcd261a7863c57933fb8fa29
bomlang <bomlang4211@gmail.com>
2023-11-24 12:52:13 +0900

    [Fix] CommentApi 매개변수로 text값과 videoId값을 받도록 수정

--- src/api/commentApi.ts
+++ src/api/commentApi.ts
@@ -1,26 +1,31 @@
-import { createClient } from "@supabase/supabase-js";
+// import { useRecoilValue } from "recoil"
+// import { videoAtom } from "@store/videoAtom";
+import { createClient } from "@supabase/supabase-js"
 
-const addComment = async () => {
+const addComment = async (text: string, video_id: string) => {
+  // const videoData = useRecoilValue(videoAtom);
+
   const supabaseAdmin = createClient(
     "https://ufinqahbxsrpjbqmrvti.supabase.co",
     process.env.REACT_APP_SUPABASE_PROJECT_API_KEY as string,
-  );
+  )
 
   try {
     const { data, error } = await supabaseAdmin.from("video_comment").insert([
       {
-        text: "test comment",
+        text,
+        video_id,
       },
-    ]);
+    ])
 
     if (error) {
-      console.error(`데이터 통신에 실패하였습니다..😵‍💫 ${error.message}`);
+      console.error(`데이터 통신에 실패하였습니다..😵‍💫 ${error.message}`)
     } else {
-      console.log("Supabase 데이터 삽입 성공:", data);
+      console.log("Supabase 데이터 삽입 성공:", data)
     }
   } catch (error) {
-    console.error(`데이터 통신에 실패하였습니다..😵‍💫 ${error}`);
+    console.error(`데이터 통신에 실패하였습니다..😵‍💫 ${error}`)
   }
-};
+}
 
-export default addComment;
+export default addComment
